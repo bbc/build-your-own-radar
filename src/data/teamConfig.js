c1d833bfc63884a1400aa4fc77a709ba54db8ff3
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2019-08-23 13:45:40 +0100

    Update team descriptions

--- src/data/teamConfig.js
+++ src/data/teamConfig.js
@@ -1,16 +1,16 @@
 const teamConfig = {
   "Education": {
-    description: "[Team description Education]",
+    description: "Education is the BBC/'s free online study support resource for school-age students in the United Kingdom.",
     slackName: '#education',
     slackLink: 'https://slack.com'
   },
   "World Service Media": {
-    description: "[Team description Education]",
+    description: "The BBC World Service, is the world's largest international broadcaster, which broadcasts radio and television news, speech and discussions in more than 40 languages",
     slackName: '#si-ws-media',
     slackLink: 'https://slack.com'
   },
   "Archive Services": {
-    description: "[Team description Education]",
+    description: "BBC Archives are collections documenting the BBC’s broadcasting history, including copies of television and radio broadcasts",
     slackName: '#archive',
     slackLink: 'https://slack.com'
   }
